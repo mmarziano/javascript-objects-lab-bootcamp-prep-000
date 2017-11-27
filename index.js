675fac19c36d2569a20f4f141bd4de0b9329a66e
Monica Marziano <mmarziano1@gmail.com>
2017-11-27 00:38:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var obj = { prop: 1 }
+  var obj = { prop: 1 };
 }
 
 function updateObjectWithKeyAndValue(obj,'prop2', 2);
