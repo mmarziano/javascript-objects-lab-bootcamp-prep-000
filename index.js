7b50b961721abddd46962d31b27d798467dabbd9
Monica Marziano <mmarziano1@gmail.com>
2017-11-27 00:52:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,12 +10,11 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
     return object;
   }
 
-function deleteFromObjectByKey(object, key) {
-  Object.assign({}, object, { key })
-  delete object[key];
-  return object;
-
-}
+  function deleteFromObjectByKey(object, key) {
+   var clone = Object.assign({}, object);
+     delete clone[key];
+     return clone;
+  }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
 
