228d5fba065313c28e15882b4c112e3ababec647
Monica Marziano <mmarziano1@gmail.com>
2017-11-27 00:38:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ function updateObjectWithKeyAndValue(object, key, value) {
   var obj = { prop: 1 }
 }
 
-function updateObjectWithKeyAndValue(obj, 'prop2', 2)
+function updateObjectWithKeyAndValue(obj,'prop2', 2);
